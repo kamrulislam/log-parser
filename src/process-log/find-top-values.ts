7ee41466145ee3fb18ea669557108b56c28e6b20
Md Kamrul Islam <kamruli@gmail.com>
2019-11-23 22:38:45 +1100

    Top three URLs and IPs

---
+++ src/process-log/find-top-values.ts
@@ -0,0 +1,17 @@
+import { MappedData } from './index.d';
+
+/**
+ * 
+ * @param values 
+ * @param howMany
+ * 
+ * This is a O(nlog(n)) solution for finding top m values
+ * 
+ * NB: this is the simplies solution. I shall add another version
+ * using modified Max Heap Algorithm which will have a time complexity
+ * of O(log(n))
+ */
+export const findTopValues = (values: MappedData[], howMany: number): MappedData[] => {
+  values.sort((valueA, valueB) => valueA.count - valueB.count);
+  return values.slice(0, howMany);
+};
--- src/process-log/process-log.ts
+++ src/process-log/process-log.ts
@@ -1,13 +1,29 @@
+import { MappedData } from '.';
 import { Log } from '../process-input';
+import { findTopValues } from './find-top-values';
 import { findUniqueIps } from './find-unique-ips';
 import { preProcessLogs } from './pre-process-logs';
 
 export const processLogs = (logs: Log[]) => {
   const preProcessedData = preProcessLogs(logs);
   // console.log(preProcessedData);
+  console.log('---------------------------------------');
   console.log('Unique IPs::');
   const uniqueIps = findUniqueIps(preProcessedData.ipMap);
   uniqueIps.forEach((ip:string, index: number) => {
-    console.log(`${index}. ${ip}`);
+    console.log(`${index + 1}. ${ip}`);
   });
+  console.log();
+  console.log('Top 3 visited Urls::');
+  const topThreeVisitedUrls = findTopValues(Array.from(preProcessedData.urlMap.values()), 3);
+  topThreeVisitedUrls.forEach((url: MappedData, index: number) => {
+    console.log(`${index + 1}. ${url.key}`);
+  });
+  console.log();
+  console.log('Top 3 most active IP::');
+  const topThreeActiveIps = findTopValues(Array.from(preProcessedData.ipMap.values()), 3);
+  topThreeActiveIps.forEach((ip: MappedData, index: number) => {
+    console.log(`${index + 1}. ${ip.key}`);
+  });
+  console.log('---------------------------------------');
 };
